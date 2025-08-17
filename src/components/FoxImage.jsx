8859cc6d33f17fc8bd82c0c95654fa77d95a60cc
Marco Guadiana <marcoguadiana01@gmail.com>
2025-08-17 15:11:45 -0700

    Add loading message while fetching data

--- src/components/FoxImage.jsx
+++ src/components/FoxImage.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
-import foxLogo from '../assets/fox-logo.png'
 import { useState, useEffect } from 'react';
+import foxLogo from '../assets/fox-logo.png'
+
 
 const API_URL = "https://randomfox.ca/floof/"
 
 function FoxImage() {
   const [image, setImage] = useState(foxLogo)
+  const [loading, setLoading] = useState(true);
   
+
+
   useEffect(() => {
     //fetch here
     fetch(API_URL)
@@ -14,16 +17,20 @@ function FoxImage() {
         if(!response.ok) { throw new Error ("Failed to fetch image"); }
         return response.json();
       })
-      .then(data => setImage(data.image))
-      .catch(error => console.log(error)); 
+      .then(data => {setImage(data.image)
+      setLoading(false);
+  })
       
+      .catch(error => console.log(error)); 
+
   }, []); 
 
 
   return (
     <div>
       <p>Learn more about us!</p>
-      <img src={image} alt="fox logo" />
+      {loading ? "" : <p> Loading...</p>}
+      <img src={image} alt="A Random Fox" />
     </div>
   );
 }
